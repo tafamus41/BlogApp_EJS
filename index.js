5d562852c7a34e8dee6b5fb170067af20aa3e79a
asimov <seyranmustafa4@gmail.com>
2025-01-11 04:05:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - EJS-BLOG Project with Mongoose
+------------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+require("express-async-errors");
+
+const session = require("cookie-session");
+const { openDelimiter } = require("ejs");
+app.use(
+  session({ secret: process.env.SECRET_KEY || "secret_keys_for_cookies" }),
+);
+/* ------------------------------------------------------- */
+// Accept json data & convert to object:
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// defaul delimeter : <% %>
+// exp delimeters : << >>, {{ }}, {% %} 
+app.set('view engine', 'ejs');
+app.set('view options', {
+  // delimiter: '%',
+  openDelimiter: "{",
+  closeDelimiter: "}"
+})
+app.set('views', './public')
+
+// Connect to MongoDB with Mongoose:
+require("./src/dbConnection");
+
+// Searching&Sorting&Pagination:
+app.use(require("./src/middlewares/queryHandler"));
+
+// StaticFiles:
+app.use("/assets", express.static("./public/assets"));
+
+// HomePage:
+app.all("/", (req, res) => {
+  res.redirect("/blog/post");
+  // res.send('<h1>Welcome to Blog APP</h1>')
+});
+
+// Routes: // VIEWS:
+app.use("/blog", require("./src/routes/view"));
+
+// Routes: // API:
+app.use("/api/blog", require("./src/routes/api"));
+
+// errorHandler:
+app.use(require("./src/middlewares/errorHandler"));
+
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
+
+// require("./src/helpers/sync2")();
