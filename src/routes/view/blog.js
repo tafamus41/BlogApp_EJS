75e2e17e5ce8198f3d9ed891597b84c9d5b327c9
asimov <seyranmustafa4@gmail.com>
2025-01-14 03:53:13 +0300

    p

--- src/routes/view/blog.js
+++ src/routes/view/blog.js
@@ -9,25 +9,16 @@ const router = require("express").Router();
 
 
 
-// Get the list of categories
+
 router.get("/", Blog.list);
 
-// Create a new category
 router.route("/create").post(Blog.create).get(Blog.create);
 
-// Get a specific category by ID
 router.get("/:id",Blog.read)
+
 router.get("/:id/delete",Blog.deleteBlog)
 
-// Update a specific category by ID
-// router.put("/:categoryId/update", Blog.update);
 router.get("/:categoryId/update", Blog.update);
-// router.patch("/:categoryId/update", Blog.update);
 
-// Delete a specific category by ID
-// router.delete("/:categoryId/delete", Blog.deleteBlog);
-
-// Get the list of posts under a specific category
-// router.get("/:categoryId/posts", Blog.listCategoryPosts);
 
 module.exports = router;
--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -16,17 +16,12 @@ router.route("/login").post(User.login).get(User.login);
 // Handle logout (POST since it involves action)
 router.get("/logout", User.logout);
 
-// User management routes
 // Get the list of users
 router.get("/", User.list);
 // Create a new user
 router.route("/register").post(User.register).get(User.register);
 // Get a specific user by ID
 router.get("/:id", User.read);
-// Update a specific user by ID
-// router.put("/:id/update", User.update);
-// router.patch("/:id/update", User.update);
-// Delete a specific user by ID
-// router.delete("/:id/delete", User.delete);
+
 
 module.exports = router;
