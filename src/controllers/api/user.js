f282bdc136e3123f2dda32773c7bfc53a1afeb17
asimov <seyranmustafa4@gmail.com>
2025-01-17 06:00:35 +0300

    p

--- src/controllers/api/user.js
+++ src/controllers/api/user.js
@@ -12,7 +12,7 @@ module.exports = {
   list: async (req, res) => {
     // const data = await User.find()
     // const data = await User.find(search).sort(sort).skip(skip).limit(limit).populate(populate)
-    const data = await res.getModelList(User);
+    const data = await res.getModelList(User,{});
 
     res.status(200).send({
       error: false,
