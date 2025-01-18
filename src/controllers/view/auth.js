1a7be9748616f3ee99d720f6bc870ea9757a4684
asimov <seyranmustafa4@gmail.com>
2025-01-18 22:13:55 +0300

    p

--- src/controllers/view/auth.js
+++ src/controllers/view/auth.js
@@ -6,7 +6,6 @@
 
 const User = require('../../models/user')
 const Token = require('../../models/token')
-const passwordEncrypt = require('../../helpers/passwordEncrypt')
 const jwt = require('jsonwebtoken')
 
 module.exports = {
