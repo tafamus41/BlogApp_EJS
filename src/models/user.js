54d30cfe93fe7870abea2a98ba8fe1b1077b457e
asimov <seyranmustafa4@gmail.com>
2025-01-18 22:28:01 +0300

    p

--- src/models/user.js
+++ src/models/user.js
@@ -30,14 +30,18 @@ const UserSchema = new mongoose.Schema({
         type: String,
         trim: true,
         required: true,
-                
+        set: (password) => passwordEncrypt(password)      
     },
 
     email: {
         type: String,
         trim: true,
         required: true, 
-        unique: true,     
+        unique: true,  
+        validate: [
+            (email) => (email.includes('@') && email.includes('.')), // ValidationCheck
+            'Email type is incorrect.' // If false Message.
+        ]  
     },
     firstName: {
         type: String,
@@ -68,28 +72,28 @@ const UserSchema = new mongoose.Schema({
 
 }, { collection: 'users', timestamps: true })
 
-UserSchema.pre(['save', 'updateOne'], function (next) {
+// UserSchema.pre(['save', 'updateOne'], function (next) {
 
-    // updateOne: _update, save: this
-    const data = this?._update ?? this
+//     // updateOne: _update, save: this
+//     const data = this?._update ?? this
 
-    // Email Validation:
-    const isEmailValidated = data.email ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) : true
+//     // Email Validation:
+//     const isEmailValidated = data.email ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) : true
 
-    if (!isEmailValidated) {
-        // throw new Error('Email is not validated');
-        next(new Error('Email is not validated'));
-    }
+//     if (!isEmailValidated) {
+//         // throw new Error('Email is not validated');
+//         next(new Error('Email is not validated'));
+//     }
 
-    const isPasswordValidated = data.password ? /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.]).{8,}$/.test(data.password) : true
+//     const isPasswordValidated = data.password ? /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.]).{8,}$/.test(data.password) : true
 
-    if (!isPasswordValidated) next(new Error('Password must be at least 8 characters long and contain at least one special character and  at least one uppercase character.'));
+//     if (!isPasswordValidated) next(new Error('Password must be at least 8 characters long and contain at least one special character and  at least one uppercase character.'));
 
-    if (data.password) data.password = passwordEncrypt(data.password)
+//     if (data.password) data.password = passwordEncrypt(data.password)
 
-    next();
+//     next();
 
-})
+// })
 
 
 /* ------------------------------------------------------- */
